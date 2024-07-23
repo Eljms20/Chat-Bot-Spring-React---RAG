d315c09a9fd878d2fe84891dbe95d6e6d36d2b03
Eljms20 <khaoulaeljamoussikh@gmail.com>
2024-07-23 01:01:21 +0100

    v1

---
+++ src/main/frontend/views/@index.tsx
@@ -0,0 +1,9 @@
+import "../styles/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function index(){
+    return(
+        <div className="p-m">
+            <h1>HOME PAGE </h1>
+        </div>
+    );
+}
---
+++ src/main/java/net/khaoula/chatbotrag/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package net.khaoula.chatbotrag.services;
+
+import org.springframework.lang.NonNullApi;
